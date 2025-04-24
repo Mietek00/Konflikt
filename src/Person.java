53db67046cdfdfc5b1945921ea37de03653486c6
unknown <wojtekjazgara7@gmail.com>
2025-04-24 14:47:11 +0200

    zmianab

--- src/Person.java
+++ src/Person.java
@@ -1,33 +1,35 @@
 public class Person {
     private String name;
     private String surname;
-
-    public Person(String name, String surname) {
-        this.name = name;
-        this.surname = surname;
-    }
+    private int wiek;
 
     public String getName() {
         return name;
-    }
-
-    public String getSurname() {
-        return surname;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
+    public String getSurname() {
+        return surname;
+    }
+
     public void setSurname(String surname) {
         this.surname = surname;
     }
 
-    @Override
-    public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                '}';
+    public int getWiek() {
+        return wiek;
+    }
+
+    public void setWiek(int wiek) {
+        this.wiek = wiek;
+    }
+
+    public Person(String name, String surname, int wiek) {
+        this.name = name;
+        this.surname = surname;
+        this.wiek = wiek;
     }
 }
